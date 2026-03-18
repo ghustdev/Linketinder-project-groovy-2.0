994569aaf8c080c98e6020f3e50cfca99f897318
ghustdev <gustavocardoso0019@gmail.com>
2026-03-18 08:48:35 -0300

    feat: organization folders and files

--- frontend-linketinder/src/area.js
+++
@@ -1,8 +0,0 @@
-// namespace Area {
-//     export function circunferencia(raio: number): number {
-//         return 2 * Math.PI * raio
-//     }
-// }
-// export function areaCircunferencia(raio: number): number {
-//     return 2 * Math.PI * raio
-// }
